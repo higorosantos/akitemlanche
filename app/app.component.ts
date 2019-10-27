c8b3dae95558b9895f9f8c5a3d6d3e4ab4b4cb80
Higor Oliveira <higorosantos@gmail.com>
2019-10-27 16:01:25 -0200

    formulario contato

--- app/app.component.ts
+++ app/app.component.ts
@@ -20,6 +20,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'Contato',
+      url: '/contato',
+      icon: 'contact'
     }
   ];
 
--- app/contato/contato.page.ts
+++ app/contato/contato.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NavController,LoadingController} from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contato',
@@ -6,10 +9,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contato.page.scss'],
 })
 export class ContatoPage implements OnInit {
+  nome: String;
+  email:String;
+  assunto: String;
+  desc: String;
+  private loading;
 
-  constructor() { }
+  constructor(public Nav: NavController, public Loading: LoadingController,public Alert: AlertController,public rota: Router) { }
 
   ngOnInit() {
   }
 
+  limparForm(){
+    this.nome = ""
+    this.email = ""
+    this.assunto = ""
+    this.desc = ""
+  }
+  
+  submitForm(){
+    var eemail = new XMLHttpRequest()
+    
+    eemail.onreadystatechange = function(){
+
+       if(this.readyState == 4 && this.status == 200){
+        console.log(this.responseText)
+
+       }
+    };     
+    eemail.open("GET",`http://18.231.120.151/app/enviarEmail.php?nome=${this.nome}&email=${this.email}&assunto=${this.assunto}&desc=${this.desc}`,true)
+    eemail.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    eemail.send();
+
+    this.Loading.create({
+      message: "Enviando..."
+    
+    }).then((overlay)=> {
+       this.loading = overlay
+       this.loading.present()
+    
+    })
+   setTimeout(()=>{
+      this.loading.dismiss();
+      this.limparForm();
+      this.alertShow();
+    },4000)
+
+
+  }
+ async alertShow(){
+  const alerta = await this.Alert.create({
+    message: "Enviado Com Sucesso",
+    buttons: ["OK"]
+    
+  })
+      alerta.present();
+      this.rota.navigate(["home"])
+  }
 }
