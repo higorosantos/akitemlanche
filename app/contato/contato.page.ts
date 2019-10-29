33a4b4faca6ac07f072e67b319be623762bfbff1
higorosantos <45833012+higorosantos@users.noreply.github.com>
2019-10-28 21:28:31 -0300

    Update contato.page.ts
    VAI DA MERDA

--- app/contato/contato.page.ts
+++ app/contato/contato.page.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import {NavController,LoadingController} from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
-
+var error;
+var resposta;
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.page.html',
   styleUrls: ['./contato.page.scss'],
 })
+
 export class ContatoPage implements OnInit {
+  
   nome: String;
   email:String;
   assunto: String;
@@ -29,13 +32,19 @@ export class ContatoPage implements OnInit {
   
   submitForm(){
     var eemail = new XMLHttpRequest()
-    
     eemail.onreadystatechange = function(){
-
+          
        if(this.readyState == 4 && this.status == 200){
-        console.log(this.responseText)
-
-       }
+         
+          console.log(this.responseText)
+            if(this.responseText == "enviado"){
+              error = false;
+              resposta = 1;
+            }else{
+              error = true;
+              resposta = 1;
+            }
+        }
     };     
     eemail.open("GET",`http://18.231.120.151/app/enviarEmail.php?nome=${this.nome}&email=${this.email}&assunto=${this.assunto}&desc=${this.desc}`,true)
     eemail.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -47,23 +56,34 @@ export class ContatoPage implements OnInit {
     }).then((overlay)=> {
        this.loading = overlay
        this.loading.present()
-    
+       console.log(resposta)
     })
-   setTimeout(()=>{
+   if(resposta == 1){
+      
       this.loading.dismiss();
-      this.limparForm();
       this.alertShow();
-    },4000)
-
+    }
 
   }
  async alertShow(){
-  const alerta = await this.Alert.create({
-    message: "Enviado Com Sucesso",
-    buttons: ["OK"]
-    
-  })
-      alerta.present();
-      this.rota.navigate(["home"])
+
+  if(error == false){
+    const alerta = await this.Alert.create({
+      message: "Enviado Com Sucesso",
+      buttons: ["OK"]
+      
+    })
+    alerta.present();
+    this.rota.navigate(["home"])
+  }
+  else{
+    const alerta = await this.Alert.create({
+      message: "Houve um erro ao enviar!",
+      buttons: ["OK"]
+    })
+    alerta.present();
+  }
+     
+     
   }
 }
