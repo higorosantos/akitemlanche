903b154bd16f157db82b35013bdb7bc4f24eca9a
higorosantos <45833012+higorosantos@users.noreply.github.com>
2019-11-01 19:53:07 -0300

    cadastro

--- app/caduser/caduser.page.ts
+++ app/caduser/caduser.page.ts
@@ -1,4 +1,9 @@
+import { Router } from '@angular/router';
+import { Usuario } from './../models/usuario';
 import { Component, OnInit } from '@angular/core';
+import { ClienteService } from '../servicos/cliente.service';
+const date = new Date();
+
 
 @Component({
   selector: 'app-caduser',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CaduserPage implements OnInit {
 
-  constructor() { }
+  data: Usuario
+  constructor(private cliente: ClienteService,
+              private router: Router,) 
+              {
+                this.data = new Usuario();
+                this.data.id = date.getTime();
+                
+               }
 
   ngOnInit() {
   }
-
+    submitForm(){
+      this.cliente.createItem(this.data).subscribe((response)=>{
+        this.router.navigate(['home'])
+      })
+    }
 }
