9c6d956357b07c741cc0d5a6e33b2f028c2c3c8c
higorosantos <45833012+higorosantos@users.noreply.github.com>
2019-11-01 19:28:25 -0300

    ll

---
+++ app/caduser/caduser.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caduser',
+  templateUrl: './caduser.page.html',
+  styleUrls: ['./caduser.page.scss'],
+})
+export class CaduserPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
